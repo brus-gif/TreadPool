e31ad03b4241614e0fa050e36469d89543bd449e
brus8683 <1863658@>
2026-03-11 12:45:53 +0800

    brus-gif修改了新的线程池代码

---
+++ firstthreadpool.cpp
@@ -0,0 +1,269 @@
+#define MAX_QUE_SIZE 2
+#include "2threadpool.h"
+
+
+ThreadPool::ThreadPool()
+	: initTreadsize_(4),
+	curfreethreadnum(0),
+	state(true),
+	Tasksize_(0),
+	Taskquemaxsize_(MAX_QUE_SIZE),
+	Mode_(PoolMode::MODE_FIXED),
+	curthreadnum(4),
+	maxThreadsize_(MAXTHREAD),
+	stew(new Steward(std::bind(&ThreadPool::stewardhandler,this)))
+{}
+ThreadPool::~ThreadPool() 
+{
+	state = false;
+	notempty.notify_all();
+	std::unique_lock<std::mutex>lock(taskquetex_);
+	while (curthreadnum>0)
+	{
+		cond.wait_for(lock, std::chrono::seconds(5), [&]()->bool {return curthreadnum == 0; });
+	}
+}
+//开启线程池
+void ThreadPool::start(int initTreadsize)
+{
+	initTreadsize_ = initTreadsize;//传入初始线程数量
+	curthreadnum = initTreadsize;//传入当前线程数量
+	for (int i = 0; i < initTreadsize; i++)
+	{
+		treads_.emplace(i,std::make_unique< Thread>(std::bind(&ThreadPool::treadhandler, this,std::placeholders::_1),i));//创建线程放到线程列表
+
+	}
+	stew->start();//管家线程启动
+
+	for (int i = 0; i < initTreadsize; i++)
+	{
+		treads_[i]->start(i);//开启所有线程
+		treads_[i]->AddId();
+
+	}
+
+}
+//设置线程池模式
+void ThreadPool::setPoolMode(PoolMode Mode)
+{
+	Mode_ = Mode;
+}
+//提交任务(任务一般在堆上建立) 
+MyResult* ThreadPool::submitTask(Task*task)
+{
+	MyResult* ptr = task->getresult();
+	if(state)
+	{
+		std::unique_lock<std::mutex>lock(taskquetex_);
+		if (notfull.wait_for(lock, std::chrono::seconds(4), [&]()->bool {return Tasksize_ <Taskquemaxsize_; })==false)
+		{
+			st.notify_one();
+			std::cerr << "The submition is false! " << std::endl;
+			return ptr;
+		}
+		que.push(task);
+		Tasksize_++;
+		std::cout << "success submition!" << std::endl;
+			notempty.notify_all();
+			if (1.5 * curfreethreadnum < Tasksize_)
+			{
+				st.notify_one();
+			}
+	}
+	else
+	{
+		std::cerr << "the threadpool has closen" << std::endl;
+	}
+	return ptr;
+}
+
+//线程函数
+void ThreadPool::treadhandler(int thread_id)
+{
+	auto lasttime = std::chrono::high_resolution_clock().now();
+
+	while (state || Tasksize_ > 0)
+	{
+		Task* task=nullptr;
+		{
+			std::unique_lock<std::mutex>lock(taskquetex_);
+			while (state&& Tasksize_ ==0)
+			{
+				curfreethreadnum++;
+				if (notempty.wait_for(lock, std::chrono::seconds(1))==std::cv_status::timeout&& Mode_== PoolMode::MODE_COACHED)//待优化点（苏醒后抢锁的时间不包含在时间计数中）
+				{
+					auto now= std::chrono::high_resolution_clock().now();
+					auto dur = std::chrono::duration_cast<std::chrono::seconds>(now - lasttime);
+					if (dur >= std::chrono::seconds(60)&& curthreadnum > initTreadsize_)//管家线程处理点
+					{
+						stew->set_destroy_threadid(thread_id);
+						curfreethreadnum--;
+						curthreadnum--;
+						return;
+					}
+				}
+				curfreethreadnum--;
+			}
+			if (!state && Tasksize_ ==0)
+			{
+				curthreadnum--;
+				if (curthreadnum == 0)
+				{
+					cond.notify_all();
+				}
+				return;
+			}
+			task = que.front();
+			que.pop();
+		Tasksize_--;
+		notfull.notify_all();
+		}
+		task->exec();
+		delete task;
+		lasttime = std::chrono::high_resolution_clock().now();
+	}
+	curthreadnum--;
+	curfreethreadnum--;
+}
+
+
+
+
+//设置任务队列的上限阈值
+void ThreadPool::setTaskquemaxsize(int Taskquemaxsize)
+{
+	if (state)
+	{
+		std::cerr << " the threadpool is working ,can't change setTaskquemaxsize" << std::endl;
+		return;
+	}
+	Taskquemaxsize_ = Taskquemaxsize;
+}
+void ThreadPool::setinitTreadsize(int initTreadsize)
+{
+	if (state)
+	{
+		std::cerr << " the threadpool is working ,can't change setinitTreadsize" << std::endl;
+		return;
+	}
+	initTreadsize_ = initTreadsize;
+	curfreethreadnum = initTreadsize;
+	curthreadnum = initTreadsize;
+}
+Thread::Thread(threadFunc fun, int id)
+{
+	func = fun;
+	threadid = id;
+}
+int Thread::AddId()
+{
+	return calculate_id++;
+}
+std::atomic_int Thread:: calculate_id = 0;
+//线程函数的开启
+void Thread::start(int id)
+{
+	std::thread t(func,id);
+	t.detach();
+}
+int Thread::getid()
+{
+	return calculate_id;
+}
+
+//MyAny::MyAny(MyAny&&val) { base = std::move(val.base); }
+//MyAny& MyAny::operator=(MyAny&& val) { base =std::move( val.base); return *this; }
+
+//管家线程函数
+void ThreadPool::stewardhandler()
+{
+	while (state)
+	{
+		std::unique_lock<std::mutex>lock(steconitor);
+
+		if (curfreethreadnum <Tasksize_ &&1.5* curfreethreadnum >=Tasksize_)
+		{
+			notempty.notify_all();
+		}
+		else if(1.5 * curfreethreadnum <Tasksize_)
+		{
+			notempty.notify_all();
+			for (int i = 0; i < 4; i++)
+			{
+				int id = Thread::AddId();
+				treads_.emplace(id, std::make_unique< Thread>(std::bind(&ThreadPool::treadhandler, this, std::placeholders::_1),id));
+				treads_[id]->start(id);
+				curthreadnum++;
+				std::cout << "add thread" << std::endl;
+			}
+
+		}
+
+
+		std::stack<int>* sck= stew->get_stack();
+		while(!sck->empty())
+		{
+			treads_.erase(sck->top());
+			sck->pop();
+			std::cout << "steward succeed destroy the thread!" << std::endl;
+		}
+		st.wait_for(lock,std::chrono::seconds(2));
+	}
+
+
+}
+std::stack<int>* Steward::get_stack()
+{
+	return &sk;
+
+}
+
+//管家线程的开启
+void Steward::start()
+{
+	std::thread t(func);
+	t.detach();
+}
+
+void Steward::set_destroy_threadid(int thread_id)
+{
+	std::unique_lock<std::mutex>lock(stex);
+	sk.push(thread_id);
+
+}
+
+
+void MyResult::setAny(MyAny&& any)
+{	
+	final1 =std::move( any);
+	sem.post();
+}
+
+MyAny MyResult::getAny()
+{
+	sem.wait_();
+	return std::move(final1);
+}
+void Task::exec()
+{
+	result->setAny(std::move(run()));
+	std::cout << "success excute!" << std::endl;
+
+}
+MyResult* Task::getresult()
+{
+	return result;
+}
+void ThreadPool::setStae(bool state_)
+{
+	state = state_;
+}
+
+
+//int main()
+//{
+//	ThreadPool pool{};
+//	pool.start(4);
+//
+//	std::this_thread::sleep_for(std::chrono::seconds(5));
+//}
---
+++ firstthreadpool.h
@@ -0,0 +1,207 @@
+#ifndef THREAD_H
+#define THREAD_H
+
+#include<iostream>
+#include<algorithm>
+#include<queue>
+#include<atomic>
+#include<mutex>
+#include<condition_variable>
+#include<memory>
+#include<functional>
+#include<type_traits>
+#include<thread>
+#include<chrono>
+#include<unordered_map>
+#include<stack>
+//#include<time.h>
+
+#define MAXTHREAD (28)//最大线程数量
+class MyAny;
+class Mysemaphore;
+//创建管家类：
+class Steward
+{
+private:
+	std::stack<int>sk;//存储待销毁的线程id
+	using stewardFunc = std::function<void()>;
+	stewardFunc func;
+	std::mutex stex;
+public:
+	Steward() {}
+	Steward(stewardFunc func_):func(func_) {}
+	~Steward(){}
+	void start();
+	void set_destroy_threadid(int thread_id);//提交待销毁线程id
+	std::stack<int>* get_stack();//销毁对应id的线程并清理vec
+};
+
+//创建Result类：
+
+//创建Any类
+class MyAny 
+{
+public:
+	MyAny() {}
+	MyAny(MyAny&)=delete;//
+	MyAny(MyAny&&)=default;//
+	MyAny& operator=( const MyAny&) = delete;
+	MyAny& operator=( MyAny&&)=default;
+template<typename T>
+MyAny(T data1) :base(new Basechild<T>(data1)){}
+~MyAny() {}
+template<typename T>
+	T cast_()
+	{
+		Basechild<T>* child = dynamic_cast<Basechild<T>*>(base.get());
+		return child->data;
+	}
+private:
+class Base 
+	{
+	public:
+		Base() {}
+		virtual ~Base() {}
+	};
+	template<typename T>
+	class Basechild:public Base
+	{
+	public:
+		Basechild() {}
+		Basechild(T data_):data(data_){}
+		Basechild& operator=(Basechild&&) = default;
+		~Basechild() {}
+		T data;
+	};
+	
+	std::unique_ptr<Base> base;
+};
+
+//semaphore信号量类的实现
+class Mysemaphore
+{
+public:
+
+	Mysemaphore(int num=0)
+		:cnt(num)
+	{}
+	void post() 
+	{
+		std::unique_lock<std::mutex>lock(setx);
+		cnt++;
+		se.notify_one();
+	}
+	void wait_() 
+	{
+		std::unique_lock<std::mutex>lock(setx);
+		se.wait(lock, [&]()->bool {return cnt > 0; });
+		cnt--;
+	}
+private:
+	std::condition_variable se;
+	std::mutex setx;
+	std::atomic<int>cnt;
+};
+class MyResult
+{
+public:
+	MyResult() {}
+	~MyResult() = default;
+	MyResult(MyResult&final_)=delete;
+	MyResult(MyResult&&final_)=default;
+	MyResult& operator=(const MyResult&final_)=delete;
+	MyResult& operator = (MyResult&&)=default;
+	void setAny(MyAny&& any);
+	MyAny getAny();
+private:
+	MyAny final1;
+	Mysemaphore sem;
+
+};
+
+//任务类:
+class Task
+{
+public:
+	Task() {}
+	Task(MyResult*result_):result(result_){}
+	virtual ~Task() { }
+	virtual MyAny run() = 0;
+	void exec();
+	MyResult* getresult();
+private:
+	MyResult* result;
+};
+
+//线程类
+class Thread
+{
+private:
+	using threadFunc = std::function<void(const int)>;
+threadFunc func;
+int threadid;
+static std::atomic_int calculate_id;
+public:
+	
+
+	//线程类的构造函数
+	Thread(threadFunc fun,int id);
+
+	//线程类构造函数（传入一个绑定器对象）
+	 void start(int id);
+	static int AddId();
+	 int getid();
+};
+
+//线程池模式类
+enum class PoolMode
+{
+	MODE_FIXED, MODE_COACHED
+};
+
+//线程池类
+class ThreadPool
+{
+private:
+	Steward *stew;//管家类对象指针
+	std::atomic_int cnt;//记录提交次数
+
+	std::unordered_map<int,std::unique_ptr<Thread>>treads_;//线程池线程列表
+	int initTreadsize_;//初始线程的数量
+	std::atomic_int curthreadnum;//当前线程数量
+	std::atomic_int curfreethreadnum;//当前空闲线程数量
+	int maxThreadsize_;//线程数量的上限阈值
+
+	std::queue<Task*>que;//任务队列
+	std::atomic_int Tasksize_;//当前队列中任务的数量
+	int Taskquemaxsize_;//任务队列上限的的阈值
+
+	std::mutex taskquetex_;//任务队列安全锁（）
+	std::condition_variable notfull;//队列不满
+	std::condition_variable notempty;//队列不空
+
+	PoolMode Mode_;//线程池模式
+	std::atomic_bool state;//记录线程池状态（开启，关闭）
+	std::condition_variable cond;//线程池析构条件变量
+	
+	std::condition_variable st;//管家线程监视
+	std::mutex steconitor;//管家监视锁
+private:
+	void treadhandler(int thread_id);//线程函数
+	void stewardhandler();//管家线程函数
+public:
+	ThreadPool();
+	~ThreadPool();
+	void start(int initTreadsize);//开启线程池
+	void setPoolMode(PoolMode Mode);//设置线程池模式
+	MyResult* submitTask(Task*task);//提交任务(任务一般在堆上建立)
+	void setTaskquemaxsize(int Taskquemaxsize);//设置任务队列的上限阈值
+	void setinitTreadsize(int initTreadsize);//设置初始线程数量
+	//删除拷贝构造和赋值重载
+	ThreadPool(ThreadPool&) = delete;
+	ThreadPool& operator=(const ThreadPool&) = delete;
+	void setStae(bool state_);
+};
+#endif
+//
+//
